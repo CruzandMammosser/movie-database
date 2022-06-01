5b1338dcac1ea823a6e32bb08527a9b34986454d
Jesse Mammosser <jesse.mamo123@gmail.com>
2022-06-01 15:04:23 -0400

    feat: loading message

--- js/constants.js
+++
@@ -1,5 +0,0 @@
-
-export const baseURL = "https://rose-summer-pencil.glitch.me/movies."
-export const fetchSettings = {
-    method: "GET", // or gets
-}
--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
+fetch('https://helpful-mercurial-eyebrow.glitch.me/movies')
+    .then(function (res) {
+        console.log(res)
+        return res.json();
+    }).then(function (data){
+        console.log(data)
+        $("#loading-image").addClass('hide')
+    // $(window).on('load', function () {
+    //     $('#loading').hide();
+    // })
 
-import {baseURL,fetchSettings} from "./constants.js";
+    });
